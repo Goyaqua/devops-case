582e71baef0e51d182cfbe1471e97edf885b801b
Bengisu <114601573+Goyaqua@users.noreply.github.com>
2026-06-06 23:07:36 +0300

    feat: add Kubernetes manifests and nginx reverse proxy config

--- mern-project/client/src/components/healthcheck.js
+++ mern-project/client/src/components/healthcheck.js
@@ -4,7 +4,7 @@ export default function HealthStatus() {
   const [status, setStatus] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:5050/healthcheck/")
+    fetch("/healthcheck/")
       .then((response) => response.json())
       .then((data) => setStatus(data));
   }, []);
